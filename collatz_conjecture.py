24582392a0d804e5471f51358750b0e8a4a6c315
Andrew Georgiou <a-georgiou@github.com>
2025-07-31 19:47:19 +0100

    Add Python script to generate the Collatz sequence

---
+++ collatz_conjecture.py
@@ -0,0 +1,23 @@
+def collatz_sequence(n):
+    """
+    Generate the Collatz (3n + 1) sequence starting from n.
+    Returns a list containing the sequence.
+    """
+    if n < 1:
+        raise ValueError("Input must be a positive integer.")
+    sequence = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+        sequence.append(n)
+    return sequence
+
+if __name__ == "__main__":
+    try:
+        num = int(input("Enter a positive integer to start the Collatz sequence: "))
+        seq = collatz_sequence(num)
+        print("Collatz sequence:", seq)
+    except ValueError as e:
+        print("Error:", e)
